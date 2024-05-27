c3b8c030a5154aa6189dec9efdaf1abdc5ecb328
Juancams <jc.manzanares.serrano@gmail.com>
2024-05-27 12:39:24 +0200

    Flake8 errors solved
    Signed-off-by: Juancams <jc.manzanares.serrano@gmail.com>

--- launch/ur_sim_control.launch.py
+++ launch/ur_sim_control.launch.py
@@ -46,113 +46,113 @@ from launch_ros.substitutions import FindPackageShare
 def launch_setup(context, *args, **kwargs):
 
     # Initialize Arguments
-    safety_limits = LaunchConfiguration("safety_limits")
-    safety_pos_margin = LaunchConfiguration("safety_pos_margin")
-    safety_k_position = LaunchConfiguration("safety_k_position")
+    safety_limits = LaunchConfiguration('safety_limits')
+    safety_pos_margin = LaunchConfiguration('safety_pos_margin')
+    safety_k_position = LaunchConfiguration('safety_k_position')
     # General arguments
-    runtime_config_package = LaunchConfiguration("runtime_config_package")
-    description_file = LaunchConfiguration("description_file")
-    launch_rviz = LaunchConfiguration("launch_rviz")
-    gazebo_gui = LaunchConfiguration("gazebo_gui")
+    runtime_config_package = LaunchConfiguration('runtime_config_package')
+    description_file = LaunchConfiguration('description_file')
+    launch_rviz = LaunchConfiguration('launch_rviz')
+    gazebo_gui = LaunchConfiguration('gazebo_gui')
 
     rviz_config_file = PathJoinSubstitution(
-        [FindPackageShare(runtime_config_package), "rviz", "view_arms.rviz"]
+        [FindPackageShare(runtime_config_package), 'rviz', 'view_arms.rviz']
     )
 
     initial_joint_controllers = PathJoinSubstitution(
-            [FindPackageShare(runtime_config_package), "config",  "ur_controllers.yaml"]
+            [FindPackageShare(runtime_config_package), 'config',  'ur_controllers.yaml']
         )
 
     nodes_to_start = []
 
     robot_description_content = Command(
         [
-            PathJoinSubstitution([FindExecutable(name="xacro")]),
-            " ",
+            PathJoinSubstitution([FindExecutable(name='xacro')]),
+            ' ',
             PathJoinSubstitution(
-                [FindPackageShare(runtime_config_package), "urdf/ur", description_file]
+                [FindPackageShare(runtime_config_package), 'urdf/ur', description_file]
             ),
-            " ",
-            "safety_limits:=",
+            ' ',
+            'safety_limits:=',
             safety_limits,
-            " ",
-            "safety_pos_margin:=",
+            ' ',
+            'safety_pos_margin:=',
             safety_pos_margin,
-            " ",
-            "safety_k_position:=",
+            ' ',
+            'safety_k_position:=',
             safety_k_position,
-            " ",
-            "name:=",
-            "ur",
-            " ",
-            "sim_gazebo:=true",
-            " ",
-            "simulation_controllers:=",
+            ' ',
+            'name:=',
+            'ur',
+            ' ',
+            'sim_gazebo:=true',
+            ' ',
+            'simulation_controllers:=',
             initial_joint_controllers,
         ]
     )
-    robot_description = {"robot_description": robot_description_content}
+    robot_description = {'robot_description': robot_description_content}
 
     robot_state_publisher_node = Node(
-        package="robot_state_publisher",
-        executable="robot_state_publisher",
-        output="both",
-        parameters=[{"use_sim_time": True}, robot_description],
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='both',
+        parameters=[{'use_sim_time': True}, robot_description],
     )
 
     joint_state_broadcaster_spawner = Node(
-        package="controller_manager",
-        executable="spawner",
-        arguments=["joint_state_broadcaster"],
+        package='controller_manager',
+        executable='spawner',
+        arguments=['joint_state_broadcaster'],
     )
 
     ur5_joint_state_broadcaster_spawner = Node(
-        package="controller_manager",
-        executable="spawner",
-        arguments=["ur5_joint_state_broadcaster"],
+        package='controller_manager',
+        executable='spawner',
+        arguments=['ur5_joint_state_broadcaster'],
     )
 
     ur10_joint_state_broadcaster_spawner = Node(
-        package="controller_manager",
-        executable="spawner",
-        arguments=["ur10_joint_state_broadcaster"],
+        package='controller_manager',
+        executable='spawner',
+        arguments=['ur10_joint_state_broadcaster'],
     )
 
     ur5_joint_trajectory_spawner = Node(
-        package="controller_manager",
-        executable="spawner",
-        arguments=["ur5_joint_trajectory_controller"],
+        package='controller_manager',
+        executable='spawner',
+        arguments=['ur5_joint_trajectory_controller'],
     )
 
     ur10_joint_trajectory_spawner = Node(
-        package="controller_manager",
-        executable="spawner",
-        arguments=["ur10_joint_trajectory_controller"],
+        package='controller_manager',
+        executable='spawner',
+        arguments=['ur10_joint_trajectory_controller'],
     )
-    
+
     # Spawn robot
     gazebo_spawn_robot = Node(
-        package="gazebo_ros",
-        executable="spawn_entity.py",
-        arguments=["-entity", "ur", "-topic", "/robot_description"],
-        output="screen",
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=['-entity', 'ur', '-topic', '/robot_description'],
+        output='screen',
     )
 
     gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            [FindPackageShare("gazebo_ros"), "/launch", "/gazebo.launch.py"]
+            [FindPackageShare('gazebo_ros'), '/launch', '/gazebo.launch.py']
         ),
         launch_arguments={
-            "gui": gazebo_gui
+            'gui': gazebo_gui
         }.items(),
     )
 
     rviz_node = Node(
-        package="rviz2",
-        executable="rviz2",
-        name="rviz2",
-        output="log",
-        arguments=["-d", rviz_config_file],
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='log',
+        arguments=['-d', rviz_config_file],
         condition=IfCondition(launch_rviz),
     )
 
@@ -171,57 +171,57 @@ def launch_setup(context, *args, **kwargs):
 
 def generate_launch_description():
     declared_arguments = []
-    runtime_config_package = LaunchConfiguration("runtime_config_package")
+    runtime_config_package = LaunchConfiguration('runtime_config_package')
     # UR specific arguments
     declared_arguments.append(
         DeclareLaunchArgument(
-            "safety_limits",
-            default_value="true",
-            description="Enables the safety limits controller if true.",
+            'safety_limits',
+            default_value='true',
+            description='Enables the safety limits controller if true.',
         )
     )
     declared_arguments.append(
         DeclareLaunchArgument(
-            "safety_pos_margin",
-            default_value="0.15",
-            description="The margin to lower and upper limits in the safety controller.",
+            'safety_pos_margin',
+            default_value='0.15',
+            description='The margin to lower and upper limits in the safety controller.',
         )
     )
     declared_arguments.append(
         DeclareLaunchArgument(
-            "safety_k_position",
-            default_value="20",
-            description="k-position factor in the safety controller.",
+            'safety_k_position',
+            default_value='20',
+            description='k-position factor in the safety controller.',
         )
     )
     # General arguments
     declared_arguments.append(
         DeclareLaunchArgument(
-            "runtime_config_package",
-            default_value="gnc_orbital",
+            'runtime_config_package',
+            default_value='gnc_orbital',
             description='Package with the controller\'s configuration in "config" folder. \
         Usually the argument is not set, it enables use of a custom setup.',
         )
     )
     declared_arguments.append(
         DeclareLaunchArgument(
-            "description_file",
-            default_value="ur.urdf.xacro",
-            description="URDF/XACRO description file with the robot.",
+            'description_file',
+            default_value='ur.urdf.xacro',
+            description='URDF/XACRO description file with the robot.',
         )
     )
     declared_arguments.append(
-        DeclareLaunchArgument("launch_rviz", default_value="true", description="Launch RViz?")
+        DeclareLaunchArgument('launch_rviz', default_value='true', description='Launch RViz?')
     )
     declared_arguments.append(
         DeclareLaunchArgument(
-            "gazebo_gui", default_value="true", description="Start gazebo with GUI?"
+            'gazebo_gui', default_value='true', description='Start gazebo with GUI?'
         )
     )
     declared_arguments.append(
         DeclareLaunchArgument(
-            "world", default_value=[PathJoinSubstitution(
-            [FindPackageShare(runtime_config_package), "worlds",  "lab.sdf"])], description="World file to load in Gazebo"
+            'world', default_value=[PathJoinSubstitution(
+            [FindPackageShare(runtime_config_package), 'worlds',  'lab.sdf'])]
         )
     )
 
